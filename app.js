320d6b2f2951bea67a940c072b75bad86d897ce4
shafakyildiz <shafakyildiz@gmail.com>
2023-11-06 20:58:30 +0300

    Add fıle system

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const app = express();
+const fs = require("fs");
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,4 +22,14 @@ app.post("/add", (req, res) => {
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
+});
+
+fs.readFile("todo.txt", "utf8", (err, data) => {
+  if (err) {
+    console.error("Error reading the file:", err);
+    return;
+  }
+  // Now you have the file content in the 'data' variable
+  // You can edit 'data' here
+  console.log(data);
 });
